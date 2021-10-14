50d77283c1921b5d766ad311efec4e67734e1134
anticher <anticherq@gmail.com>
2021-10-14 18:53:07 +0300

    feat: complete sq-queue task

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -15,18 +15,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 module.exports = class Queue {
 
+
+
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    return this.queue
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    if (!this.queue) {
+      this.queue = new ListNode(value)
+    } else {
+      let current = this.queue
+      while (current.next) {
+        current = current.next
+      }
+      current.next = new ListNode(value)
+    }
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    let result = this.queue.value
+    if (this.queue.next) {
+      let nextNode = this.queue.next
+      this.queue.value = nextNode.value
+      this.queue.next = nextNode.next
+    } else {
+      this.queue.value = null
+    }
+    return result
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
