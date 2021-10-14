2bf7da85dd63d4c5bcd729edbce9769af662dbc6
anticher <anticherq@gmail.com>
2021-10-14 17:28:07 +0300

    feat: complete st-remove-from-list task

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
+module.exports = function removeKFromList(l, k) {
+  if (l === null) {
+    return l
+  }
+  if (l.value === k) {
+    l = l.next
+  }
+  let currentNode = l
+  let nextNode = l.next
+  while(nextNode !== null){
+    if(nextNode.value === k){
+        currentNode.next = nextNode.next
+        if(currentNode.next === null)
+            break;
+    }
+    currentNode = currentNode.next
+    nextNode = currentNode.next
+  }
+  return l
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
